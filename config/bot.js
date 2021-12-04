cfc46eeb6f2ac3f35946819293e8f9c71f164226
Teo553 <81677136+Teo553@users.noreply.github.com>
2021-12-04 19:33:37 +0100

    Add files via upload

---
+++ config/bot.js
@@ -0,0 +1,17 @@
+module.exports = {
+    emojis: {
+        off: ':x:',
+        error: ':warning:',
+        queue: ':bar_chart:',
+        music: ':musical_note:',
+        success: ':white_check_mark:',
+    },
+
+    discord: {
+        token: 'ODI2NzA4Mjc2NDU1OTMxOTI2.YGQaFA.BJslh24rq081FsR0pWf1dKCio6E',
+        prefix: '!',
+        activity: 'ACKA FOR LIFE',
+    },
+
+    filters: ['8D', 'gate', 'haas', 'phaser', 'treble', 'tremolo', 'vibrato', 'reverse', 'karaoke', 'flanger', 'mcompand', 'pulsator', 'subboost', 'bassboost', 'vaporwave', 'nightcore', 'normalizer', 'surrounding'],
+};
